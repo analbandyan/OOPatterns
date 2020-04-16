161884388fdf49e027c6d448abaa557c5ec4a526
Ashot <ashot.nalbandyan@mentorcliq.com>
2020-04-16 12:51:54 +0400

    prepare for the 2nd part of singletons

--- src/com/patterns/singleton/SingletonRunner.java
+++ src/com/patterns/singleton/SingletonRunner.java
@@ -1,9 +1,6 @@
 package com.patterns.singleton;
 
-import com.patterns.singleton.impl.Nton;
 import com.patterns.singleton.impl.SingletonClassic;
-
-import java.util.stream.Stream;
 
 import static java.lang.System.out;
 
@@ -11,7 +8,6 @@ public class SingletonRunner {
 
     public static void run() throws Exception {
         test();
-//        testNton();
     }
 
     private static void test() {
@@ -38,14 +34,6 @@ public class SingletonRunner {
         out.println("instance 1 = " + inst1);
         out.println("instance 2 = " + inst2);
 
-    }
-
-
-
-    private static void testNton() {
-        Stream.generate(Nton::getInstance)
-                .limit(20)
-                .forEach(ntonInstance -> out.println("Nton " + ntonInstance.getInstanceNum()));
     }
 
 }
